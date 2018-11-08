9a1c57a990116e900c6c6c47e75b5ef7d5e98aec
aditya sadhu <aditya.hf96@gmail.com>
2018-11-08 10:02:41 -0500

    initial commit

---
+++ src/main/java/com/example/ChatApplicationJavaSpringBoot/ChatApplicationJavaSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.example.ChatApplicationJavaSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApplicationJavaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatApplicationJavaSpringBootApplication.class, args);
+	}
+}
